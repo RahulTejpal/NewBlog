755522ba0bb1ea54f3a0a760c4a54f80e75b3a5b
RahulTejpal <rahul.tejpal@iiitb.ac.in>
2023-04-08 11:07:44 +0530

    controllers,models and routes

---
+++ server/controllers/userController.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const colors = require('colors');
+
+//@route POST api/users/register
+// @desc Register a user
+//@access public
+const registerUser = async (req,res) => {
+    res.send('Register a user');
+}
+
+//@route POST api/users/login
+//@desc Register a user
+//@access Public
+const loginUser = async (req,res) => {
+    res.send('Login a user');
+}
+
+//@route GET api/users/profile
+//@desc Get user profile
+//@access Private
+const getProfile = async (req,res) => {
+    res.send('Get user profile');
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getProfile
+} 
--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 const express=require('express');
 const colors=require('colors');
-const mongoose=require('mongoose');
-const PORT=process.env.PORT || 5000;
+const mongoose=require('mongoose'); //helps in connecting to mongodb database
+const PORT=process.env.PORT || 5000; //server port =5000
 const dotenv=require('dotenv').config();
 
 const app=express();
 
-app.use(express.json({extended:false}));
+app.use(express.json({extended:false})); //ADD NOTES =>Middlewares
 
-const username=process.env.DB_USERNAME;
-const password=process.env.DB_PASSWORD;
-const URL=`mongodb+srv://${username}:${password}@blogapp.hlgonf7.mongodb.net/?retryWrites=true&w=majority`;
+const username=process.env.DB_USERNAME;//environment variable from .env file
+const password=process.env.DB_PASSWORD;//environment variable from .env file
+const URL=`mongodb+srv://${username}:${password}@blogapp.hlgonf7.mongodb.net/?retryWrites=true&w=majority`; // using environment variables to hide sensitive info like dab credentials(hardcoding them can pose security issues if code is ever shared or exposed)
 
-const connectDB=async()=>{
+const connectDB=async()=>{ //connection to db
     try{
-        const conn=await mongoose.connect(URL,{useNewUrlParser:true});
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+        const conn=await mongoose.connect(URL,{useNewUrlParser:true}); //func called with constructed URL and some connection options
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold); //sucessful connection => o/p = hostname of connected MongoDB instance
     }catch(error){
         console.log(`ERROR: ${error.message}`.bgRed.underline.bold);
         process.exit(1);
@@ -24,6 +24,6 @@ const connectDB=async()=>{
 
 connectDB();
 
-
+app.use ('/api/users', require('./routes/userRoutes')); //when we call /api/users our server is going to recieve it, it's going to look into our routes folder, it's going to pull up userRoutes and userRoutes will act like a traffic cop as it will forward it to wherever it needs to go
 
 app.listen(PORT,()=>console.info(`Server in running on port ${PORT}`.bgGreen.underline.bold));
