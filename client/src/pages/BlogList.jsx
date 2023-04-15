d78857d55de26302d20023a0f6072b3c44ac3533
RahulTejpal <rahul.tejpal@iiitb.ac.in>
2023-04-15 16:34:00 +0530

    Menu and tooltip

--- client/src/pages/BlogList.jsx
+++ client/src/pages/BlogList.jsx
@@ -1,6 +1,12 @@
 import React,{useState,useEffect} from 'react'
 
+import {
+    Grid, Paper,Typography, Button, Container, Stack, Tooltip,Box, List,ListItem, ListItemText
+} from '@mui/material'
 
+import Masonry from '@mui/lab/Masonry'
+
+import { useNavigate, Link } from 'react-router-dom'
 
 import { useBlog } from '../middleware/contextHooks'
 import MainContainer from '../components/MainContainer'
@@ -8,6 +14,7 @@ import { toast } from 'react-toastify';
 
 export default function BlogList(){
     const {getBlogs,toasts,clearErrors,blogs} = useBlog();
+    const navigate = useNavigate();
     const [myBlogs,setMyBlogs] = useState([]);
 
     useEffect(()=> {
@@ -26,7 +33,30 @@ export default function BlogList(){
     })
     return(
         <MainContainer>
-            {'There are ${myBlogs.length} blogs'}
+           <Container maxWidth="lg" sx={{py: 1,my:1}}>
+                <Grid container spacing={2}>
+                    <Grid item xs = {false} md={3}>
+                        
+                            <Stack spacing={2} sx={{display: 'flex'}} direction='row'>
+                                <Box sx={{flexGrow: 1}} />
+                                <Button fullWidth={false} onClick={()=> navigate('/newblog')}>Create Blog</Button>
+                            </Stack>
+
+                            <List>
+                                {myBlogs.map(blog=> (
+                                    <Link to={'/blogs/${blog._id}'} key={blog._id}>
+                                        <ListItem>
+                                            <Tooltip title={blog.title} >
+                                                <ListItemText primary={blog.title} />
+                                            </Tooltip>
+                                        </ListItem>
+                                    </Link>
+                                ))}
+                            </List>
+                        
+                    </Grid>
+                </Grid>
+           </Container>
         </MainContainer>
     )
 }
