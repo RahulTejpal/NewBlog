32eb6dbcc0d551a0bfdb36a1dc2d9e05affa9854
RahulTejpal <rahul.tejpal@iiitb.ac.in>
2023-04-26 13:41:12 +0530

    commnit

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -12,7 +12,7 @@ const logger = createLogger({
   transports: [
     new transports.Console(),
     new winston.transports.File({
-      filename: "/usr/src/app/logs/BlogLog.log",
+      filename: "/usr/src/app/logs/Blog-log.log",
       level: "info",
     }),
   ],
